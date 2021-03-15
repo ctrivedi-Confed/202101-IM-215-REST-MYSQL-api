13c32fb8aa6087c2c5380c73446e84bba82382fb
Chintan Trivedi <chintantrivedi247@gmail.com>
2021-03-14 23:43:25 -0400

    with insommnia display data

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+const { response } = require('express');
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (request, response) => {
 
     // response.json({msg: 'received1'});
+    //console.log(request.query);
+
+    //console.log(request.params);
+
+    console.log(request.body);
+    // response.end();
+    response.end();
+})
+
+app.get('/:firstName/:lastName/:age', (request, response) => {
+    console.log(request.params);
     response.end();
 })
 
